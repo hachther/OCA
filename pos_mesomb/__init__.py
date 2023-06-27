b58234acf9c1b72b0019f3ec6bd2ca20242a1ea5
hachther <hachther@gmail.com>
2023-06-27 19:02:25 +0100

    Fixing some bugs

--- pos_mesomb/__init__.py
+++ pos_mesomb/__init__.py
@@ -1,7 +1,1 @@
 from . import models
-
-def _pre_init_mesomb(cr):
-    import subprocess
-    import sys
-    print('fisher rbink')
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pymesomb'])
